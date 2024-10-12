4430362a03ffa5ac1198fc187f8ae1d22837670f
dsfgdsfgsdfg456456 <humairahmed.allfree@gmail.com>
2024-10-12 23:41:20 +0500

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,8 +1,8 @@
 // List of URLs
 const urls = [
-    "https://example1.com",
-    "https://example2.com",
-    "https://example3.com"
+    "https://alcashzone.com",
+    "https://alcashedu.com",
+    "https://youtube.com/@alcashzone"
 ];
 
 // Redirect to a random URL
